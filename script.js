40e941176c0748eb9361f8018d1c77070c0d6f3e
Dipanjan-pixel <dipanjanghosh330@gmail.com>
2024-07-26 21:45:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function appendCharacter(character) {
+    const display = document.getElementById('display');
+    if (display.innerText === '0') {
+        display.innerText = character;
+    } else {
+        display.innerText += character;
+    }
+}
+
+function clearDisp() {
+    document.getElementById('display').innerText = '0';
+}
+
+function deleteDigit() {
+    const display = document.getElementById('display');
+    if (display.innerText.length > 1) {
+        display.innerText = display.innerText.slice(0, -1);
+    } else {
+        display.innerText = '0';
+    }
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
